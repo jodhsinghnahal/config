8e7647c74ca1a888f8411eb4533f4bdb22041135
jodh <nahal.jodhsingh@gmail.com>
2024-12-17 15:38:57 -0800

    jk

--- nvim/.config/nvim/lua/jodh/core/keymaps.lua
+++ nvim/.config/nvim/lua/jodh/core/keymaps.lua
@@ -7,13 +7,15 @@ local keymap = vim.keymap -- for concisenss
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("c", "jk", "<C-c>", { desc = "Exit command mode with jk" })
+-- keymap.set("t", "jk", "<ESC>", { desc = "Exit command mode with jk" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
-keymap.set("n", "d", '"_d')
+-- keymap.set("n", "d", '"_d')
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
@@ -23,7 +25,11 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- keymap.set("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sq", "<cmd>q<CR>", { desc = "Close current split" }) -- close current split window
+-- keymap.set("n", "<leader>sa", "<cmd>qa<CR>", { desc = "Close current split" }) -- close current split window
+-- keymap.set("n", "<leader>sx", "<cmd>x<CR>", { desc = "Save and close current split" }) -- close current split window
+-- keymap.set("n", "<leader>sw", "<cmd>w<CR>", { desc = "Save current split" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tq", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
